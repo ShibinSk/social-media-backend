cecee18757d40da9358d6b839e3e2708f644cafd
shibinSk <shibinskombanal@gmail.com>
2023-04-21 00:30:19 +0530

    y

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ import { users, posts } from "./data/index.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+app.use(cors());
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -29,7 +30,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
