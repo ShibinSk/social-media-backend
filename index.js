bd2a33883347542e4bfedf3ff8e065ce6acd719f
shibinSk <shibinskombanal@gmail.com>
2023-04-23 15:04:18 +0530

    n

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
+const app = express();
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 app.use(cors());
-const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
